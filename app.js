eb3d8e8c23b5cd5fd032d2504863bdb94748c01b
Seungwoo Yi <63825580+sw08@users.noreply.github.com>
2024-05-04 12:27:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -72,8 +72,10 @@ for (const car of fs.readdirSync(temp)) {
     }
 }
 
-for (const img of fs.readdirSync(path.join(__dirname, 'carImages'))) {
-    carImages[img.split('.')[0]] = path.join(__dirname, 'carImages', img);
+if (fs.existsSync(path.join(__dirname, 'carImages'))) {
+    for (const img of fs.readdirSync(path.join(__dirname, 'carImages'))) {
+        carImages[img.split('.')[0]] = path.join(__dirname, 'carImages', img);
+    }
 }
 
 const app = express();
@@ -113,19 +115,21 @@ app.get('/:page', async (req, res) => {
         return;
     }
     const records = await db.getRankingByPage(req.params.page);
-    for (let i = 0; i < records.length; i++) {
-        if (!(records[i].model in carNames)) {
-            try {
-                const route = path.join(config.SERVERDIR, 'content/cars', records[i].model, 'ui/ui_car.json');
-                const ui_car = JSON.parse((fs.readFileSync(route, encoding = 'utf8')).replaceAll('\n', ''));
-                carNames[records[i].model] = ui_car.name;
-            } catch (Err) {
-                carNames[records[i].model] = records[i].model;
+    if (records) {
+        for (let i = 0; i < records.length; i++) {
+            if (!(records[i].model in carNames)) {
+                try {
+                    const route = path.join(config.SERVERDIR, 'content/cars', records[i].model, 'ui/ui_car.json');
+                    const ui_car = JSON.parse((fs.readFileSync(route, encoding = 'utf8')).replaceAll('\n', ''));
+                    carNames[records[i].model] = ui_car.name;
+                } catch (Err) {
+                    carNames[records[i].model] = records[i].model;
+                }
             }
+            records[i].modelName = carNames[records[i].model];
         }
-        records[i].modelName = carNames[records[i].model];
     }
-    if (records === undefined && req.params.page != 0) res.redirect('back');
+    if (records === undefined && req.params.page != 0) res.redirect('/');
     else res.render('ranking', { records: records || [], title: config.WEBTITLE, track: db.trackName, page: Number(req.params.page) });
 });
 
