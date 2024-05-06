4e6905e331bcb0f9d6fc7431d8a513ebc8fede56
Seungwoo Yi <63825580+sw08@users.noreply.github.com>
2024-05-06 20:11:44 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,122 +1,154 @@
 const fs = require("fs");
 const path = require("path");
-const sql = require('sqlite3').verbose();
-const ini = require('ini');
-const cache = require('node-cache');
+const server = require('acserver-plugin');
+const db = new (require('./db'))();
+const tools = require('./tools');
 
-class DB {
-    init(serverDir, dbFileDir) {
-        this.config = ini.parse(fs.readFileSync(path.join(serverDir, 'cfg', 'server_cfg.ini'), 'utf-8'));
-        this.connection = [];
-        for (let i = 0; i < this.config.SERVER.MAX_CLIENTS; i++) {
-            this.connection.push({
-                guid: undefined,
-                laptime: undefined,
-                model: undefined,
-                car_id: i,
-                laps: undefined,
-                name: undefined,
-            });
+const config = JSON.parse(fs.readFileSync('./config.json'));
+const pluginApp = new server.PluginApp();
+
+db.init(config.SERVERDIR, config.DBDIR);
+
+
+pluginApp.on(server.PROTOCOLS.NEW_CONNECTION, async (data) => {
+    await db.updateUsername(data.guid, data.name);
+    const record = await db.getRecord(data.guid) || { laptime: 0, laps: 0 };
+    db.addConnection(data.car_id, data.guid, record.laptime, data.model, record.laps, data.name);
+});
+
+pluginApp.on(server.PROTOCOLS.CLIENT_LOADED, async (data) => {
+    const record = await db.getRecord(db.getConnection(data.car_id).guid);
+    if (record && record.laptime !== 0) {
+        pluginApp.sendChat(data.car_id, `현재 랩타임: ${tools.formatLaptime(record.laptime)} / ${db.getCarName(record.model)}`);
+    }
+});
+
+pluginApp.on(server.PROTOCOLS.CHAT, async (data) => {
+    if (['!랩타임', "!laptime"].includes(data.message)) {
+        const record = await db.getRecord(db.getConnection(data.car_id).guid);
+        if (record && record.laptime !== 0) {
+            pluginApp.sendChat(data.car_id, `현재 랩타임: ${tools.formatLaptime(car.laptime)} / ${db.getCarName(record.model)}`);
+        } else {
+            pluginApp.sendChat(data.car_id, '현재 기록된 랩타임이 없습니다.');
         }
-        try {
-            const track = JSON.parse(fs.readFileSync(path.join(serverDir, 'content', 'tracks', this.config.SERVER.TRACK, 'ui', 'ui_track.json')));
-            this.trackName = track.name;
-        } catch {
-            this.trackName = this.config.SERVER.TRACK;
+    }
+});
+
+pluginApp.on(server.PROTOCOLS.CONNECTION_CLOSED, async (data) => {
+    db.updateLaps(data.car_id);
+    db.removeConnection(data.car_id);
+});
+
+pluginApp.on(server.PROTOCOLS.LAP_COMPLETED, async (data) => {
+    db.addLap(data.car_id);
+    if (data.cuts > 0) return;
+    if (data.laptime < config.MINLAPTIME) return;
+    const record = db.getConnection(data.car_id);
+    if (record.laptime === 0) {
+        pluginApp.sendChat(data.car_id, `개인 랩타임 갱신: ${tools.formatLaptime(data.laptime)}`);
+        db.addLaptime(data.car_id, record.guid, record.model, data.laptime);
+    } else if (data.laptime < record.laptime) {
+        pluginApp.sendChat(data.car_id, `개인 랩타임 갱신: ${tools.formatLaptime(data.laptime)} (${(data.laptime - record.laptime) / 1000}s)`);
+        db.updateLaptime(data.car_id, record.guid, record.model, data.laptime);
+    }
+});
+
+pluginApp.on(server.PROTOCOLS.CHAT, async (data) => {
+    if (db.getConnection(data.car_id).guid !== config.OWNERGUID) return;
+    if (data.message.startsWith('!set')) {
+        const guid = data.message.split(' ')[1];
+        const laptime = data.message.split(' ')[2];
+        const model = data.message.split(' ')[3];
+        if (await db.getRecord(guid)) {
+            db.db.run('update record set laptime=?, model=? where guid=?', [laptime, model, guid]);
+        } else {
+            const name = data.message.split(' ')[4];
+            db.db.run('insert into record VALUES (?, ?, ?, ?, ?)', [guid, name, model, laptime, 1])
         }
-        this.db = new sql.Database(path.join(dbFileDir, this.config.SERVER.TRACK + '.db'));
-        this.db.serialize(() => {
-            this.db.run('create table if not exists record (guid INTEGER PRIMARY KEY, name TEXT, model TEXT, laptime INTEGER, laps INTEGER)');
-            this.db.run('create index if not exists laptimeIndex on record(laptime ASC)');
-        });
-        this.cache = new cache({ stdTTL: 1800 });
-        this.ranking = new cache({ stdTTL: 600, checkperiod: 300 });
-        this.carNames = {};
     }
-    addLap(car_id) {
-        this.connection[car_id].laps++;
-    }
-    removeConnection(car_id) {
-        this.connection[car_id] = {
-            guid: undefined,
-            laptime: undefined,
-            model: undefined,
-            car_id: car_id,
-            laps: undefined,
-            name: undefined
-        };
-    }
-    addConnection(car_id, guid, laptime, model, laps, name) {
-        this.connection[car_id] = {
-            guid: guid,
-            laptime: laptime,
-            model: model,
-            car_id: car_id,
-            laps: laps,
-            name: name
-        };
-    }
-    getConnection(car_id) {
-        return this.connection[car_id];
-    }
-    getRecord(guid) {
-        return new Promise((resolve, reject) => {
-            const cacheRecord = this.cache.get(guid);
-            if (cacheRecord !== undefined) resolve(cacheRecord);
-            this.db.get(`select * from record where guid = ${guid}`, (err, row) => {
-                this.cache.set(guid, row);
-                resolve(row);
-            });
-        });
-    }
-    updateUsername(guid, name) {
-        return new Promise((resolve, reject) => {
-            this.db.run('update record set name=? where guid=?', [name, guid], (err) => {
-                resolve();
-            });
-        });
-    }
-    addLaptime(car_id, guid, model, laptime) {
-        this.connection[car_id].laptime = laptime;
-        const conn = this.getConnection(car_id);
-        this.db.run('insert into record VALUES (?, ?, ?, ?, ?)', [guid, conn.name, model, laptime, conn.laps])
-    }
-    updateLaptime(car_id, guid, model, laptime) {
-        this.connection[car_id].laptime = laptime;
-        this.db.run('update record set laptime=?, model=? where guid=?', [laptime, model, guid]);
-        this.cache.del(guid);
-    }
-    updateLaps(car_id) {
-        const conn = this.getConnection(car_id);
-        this.db.run('update record set laps=? where guid=?', [conn.laps, conn.guid]);
-    }
-    getCarName(model) {
-        if (!(model in this.carNames)) {
-            try {
-                const car = JSON.parse(fs.readFileSync(path.join(serverDir, 'content', 'cars', model, 'ui', 'ui_car.json')));
-                this.carNames[model] = car.name;
-            } catch {
-                this.carNames[model] = model;
-            }
-        }
-        return this.carNames[model];
-    }
-    close() {
-        this.db.close();
-    }
-    getRankingByPage(page) {
-        return new Promise((resolve, reject) => {
-            const cacheRanking = this.ranking.get(page);
-            if (cacheRanking !== undefined) resolve(cacheRanking);
-            this.db.all(`select name, model, laptime, laps from record order by laptime asc`, (err, rows) => {
-                for (let i = 0; i < parseInt(rows.length / 25) + (rows.length % 25 === 0 ? 0 : 1); i++) {
-                    if (rows.length < 25 * (i + 1)) this.ranking.set(i, rows.slice(25 * i));
-                    else this.ranking.set(i, rows.slice(25 * i, 25 * i + 25));
-                }
-                resolve(this.ranking.get(page));
-            });
-        });
+});
+
+pluginApp.run(config.UDPPORT);
+
+process.on('exit', () => {
+    db.close();
+});
+
+
+const express = require('express');
+
+const carImages = {};
+const carNames = {};
+
+let temp = path.join(config.SERVERDIR, 'content/cars');
+for (const car of fs.readdirSync(temp)) {
+    try {
+        fs.writeFileSync(path.join(temp, car, 'ui/ui_car.json'), fs.readFileSync(path.join(temp, car, 'ui/ui_car.json'), encoding = 'utf8').replaceAll('\\r\\n', '').replaceAll('\t', ''));
+    } catch (Err) {
     }
 }
 
-module.exports = DB;
+if (fs.existsSync(path.join(__dirname, 'carImages'))) {
+    for (const img of fs.readdirSync(path.join(__dirname, 'carImages'))) {
+        carImages[img.split('.')[0]] = path.join(__dirname, 'carImages', img);
+    }
+}
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './');
+
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.redirect('/0');
+});
+
+app.get('/img/:carName', (req, res) => {
+    if (req.params.carName in carImages) {
+        res.sendFile(carImages[req.params.carName]);
+    } else {
+        try {
+            const route = path.join(config.SERVERDIR, 'content/cars', req.params.carName, 'skins/');
+            const skins = fs.readdirSync(route);
+            carImages[req.params.carName] = path.join(route, skins[0], 'preview.jpg');
+        } catch (err) {
+            res.sendFile(path.join(__dirname, 'public', 'default.jpg'));
+        }
+    }
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/0');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:page', async (req, res) => {
+    if (Number(req.params.page) == NaN) {
+        res.redirect('/');
+        return;
+    }
+    const records = await db.getRankingByPage(req.params.page);
+    if (records) {
+        for (let i = 0; i < records.length; i++) {
+            if (!(records[i].model in carNames)) {
+                try {
+                    const route = path.join(config.SERVERDIR, 'content/cars', records[i].model, 'ui/ui_car.json');
+                    const ui_car = JSON.parse((fs.readFileSync(route, encoding = 'utf8')).replaceAll('\n', ''));
+                    carNames[records[i].model] = ui_car.name;
+                } catch (Err) {
+                    carNames[records[i].model] = records[i].model;
+                }
+            }
+            records[i].modelName = carNames[records[i].model];
+        }
+    }
+    if (records === undefined && req.params.page != 0) res.redirect('/');
+    else res.render('ranking', { records: records || [], title: config.WEBTITLE, track: db.trackName, page: Number(req.params.page) });
+});
+
+app.listen(config.WEBPORT, () => {
+    console.log('web listening on port ' + config.WEBPORT);
+});
